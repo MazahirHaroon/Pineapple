d90dbe604f1bc96b12ed2232e0eb0cebb2d50e04
BoyWithSilverWings <agney.menons@live.com>
2017-11-26 11:52:57 +0530

    Search

--- ui/src/Doctor/index.js
+++ ui/src/Doctor/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import PatientSearch from "./PatientSearch";
+import DocProfile from "./DocProfile";
 import DocView from "./DocView";
+import { Route } from "react-router-dom";
 
 class Doctor extends React.Component {
   render() {
-    return <DocView />;
+    return (
+      <section>
+        <Route path="/" component={PatientSearch} />
+        <Route path="/patient" component={DocProfile} />
+        <Route path="/view" component={DocView} />
+      </section>
+    );
   }
 }
 
